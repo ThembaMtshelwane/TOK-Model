8cc6b689077eeb98b23ac73fca9dd12a4bc412e5
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-06 01:32:21 +0200

    convolution

--- api/controllers/system.py
+++ api/controllers/system.py
@@ -1,5 +1,22 @@
+import numpy as np
 
 def best_impulse_response():
     # Simulating computation of the best impulse response
     impulse_response = [1, 2, 3, 4, 5]
     return impulse_response
+
+
+def convolution(input_array, impulse_response):
+    """
+    Computes the convolution of an input array with an impulse response.
+
+    Args:
+        input_array (list): The input array (signal).
+        impulse_response (list): The impulse response array.
+
+    Returns:
+        list: The result of the convolution.
+    """
+    # Perform the convolution using numpy
+    result = np.convolve(input_array, impulse_response, mode='full')
+    return result.tolist()
--- api/routes/system_routes.py
+++ api/routes/system_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from api.controllers.system import best_impulse_response
+from flask import Blueprint, request, jsonify
+from api.controllers.system import best_impulse_response,convolution
 
 system_routes = Blueprint('system_routes', __name__)
 
@@ -7,3 +7,17 @@ system_routes = Blueprint('system_routes', __name__)
 def get_best_impulse_response():
     response = best_impulse_response()
     return jsonify({"impulse_response": response})
+
+# Route to compute convolution
+@system_routes.route('/convolution', methods=['POST'])
+def compute_convolution():
+    data = request.get_json()
+    input_array = data.get('input_array', [])
+    impulse_response = data.get('impulse_response', [])
+    
+    if not input_array or not impulse_response:
+        return jsonify({'error': 'Both input_array and impulse_response are required'}), 400
+
+    # Compute convolution
+    result = convolution(input_array, impulse_response)
+    return jsonify({'result': result})
