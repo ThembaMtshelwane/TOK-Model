40017cdda487eb8418436bf7982a1fef174c9023
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-06 01:17:43 +0200

    cors added

--- api/app.py
+++ api/app.py
@@ -5,9 +5,11 @@ import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from flask import Flask
+from flask_cors import CORS
 from api.routes.system_routes import system_routes
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(system_routes, url_prefix='/systems')
 
